d4563d9e3f343f21fc561fd98e94c51ba6edf1e9
Dan Feigin <dfeigin@nvidia.com>
2026-07-28 15:22:52 +0300

    Revert "restore: update prev_vma_end after processing the last vma in a list"
    This reverts commit 841d50ffbed9761f90925bb000d40406f5f4bc48.

--- criu/cr-restore.c
+++ criu/cr-restore.c
@@ -2608,6 +2608,7 @@ static long restorer_get_vma_hint(struct list_head *tgt_vma_list, struct list_he
 			}
 
 			s_vma = vma_next(s_vma);
+			continue;
 		}
 
 		if (prev_vma_end + vma_len > t_vma->e->start) {
@@ -2628,6 +2629,7 @@ static long restorer_get_vma_hint(struct list_head *tgt_vma_list, struct list_he
 			}
 
 			t_vma = vma_next(t_vma);
+			continue;
 		}
 
 		return prev_vma_end;
